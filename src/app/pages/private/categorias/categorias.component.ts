4e6de2d53d8008c2daf48dff0b7c0307cb579aa5
Joao Marcelo de Jesus Macedo <joaomacedo.millenium@poupex.com.br>
2022-01-08 17:16:18 -0300

    Reorganizado a pagina de categorias

--- src/app/pages/private/categorias/categorias.component.ts
+++ src/app/pages/private/categorias/categorias.component.ts
@@ -11,7 +11,32 @@ export class CategoriasComponent implements OnInit {
 
   processandoRequisicao: boolean
 
+  paginaACarregar: number
+
+  activeRec = ""
+  activeDes = ""
+
   ngOnInit(): void {
+    this.paginaACarregar = 0
+    this.activeRec = "active"
+  }
+
+  tipoCategoria(selecao: number) {
+    
+    this.paginaACarregar = selecao
+
+    if (selecao == 0) {
+      this.activeRec = "active"
+    } else {
+      this.activeRec = ""
+    }
+
+    if (selecao == 1) {
+      this.activeDes = "active"
+    } else {
+      this.activeDes = ""
+    }
+
   }
 
 }
